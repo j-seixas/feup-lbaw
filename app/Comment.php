4e33c619afbd85e9ea1cb55b47e63923bd4dc99f
rafaeldamasceno <rafaeldamasceno@me.com>
2018-04-17 18:59:12 +0100

    Change item to comment.

--- app/Comment.php
+++ app/Comment.php
@@ -4,15 +4,15 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Item extends Model
+class Comment extends Model
 {
   // Don't add create and update timestamps in database.
   public $timestamps  = false;
 
   /**
-   * The card this item belongs to.
+   * The comment this item belongs to.
    */
-  public function card() {
-    return $this->belongsTo('App\Card');
+  public function comment() {
+    return $this->belongsTo('App\Event');
   }
 }
