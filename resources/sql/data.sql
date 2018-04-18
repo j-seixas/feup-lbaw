c7e4aba0681bf4b6c17d3ef627f7e363b486acc2
rafaeldamasceno <rafaeldamasceno@me.com>
2018-04-18 09:28:40 +0100

    Add sequence values

--- resources/sql/data.sql
+++ resources/sql/data.sql
@@ -1,3 +1,10 @@
+SELECT pg_catalog.setval('comment_id_seq', 51, false);
+SELECT pg_catalog.setval('country_id_seq', 246, false);
+SELECT pg_catalog.setval('event_id_seq', 11, false);
+SELECT pg_catalog.setval('member_id_seq', 51, false);
+SELECT pg_catalog.setval('notification_id_seq', 21, false);
+SELECT pg_catalog.setval('option_id_seq', 31, false);
+
 INSERT INTO country VALUES (1, 'Afghanistan');
 INSERT INTO country VALUES (2, 'Albania');
 INSERT INTO country VALUES (3, 'Algeria');
