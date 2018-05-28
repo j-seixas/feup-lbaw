068aea5b710d1d20e2f1b79555c3f1d64602eaec
João Seixas <jnuno1997@gmail.com>
2018-05-28 17:20:02 +0100

    comments done

--- resources/views/pages/event.blade.php
+++ resources/views/pages/event.blade.php
@@ -70,7 +70,9 @@
                 </div>
 
                 <ul class="list-group list-group-flush">
-                    @each('partials.comment', $comments, 'comment')
+                    @foreach($comments as $comment)
+                        @include('partials.comment')
+                    @endforeach
                     <li class="list-group-item p-0">
                         <div class="container pb-0 pl-3 pt-2 pr-1">
                             <img alt="Profile picture" src="https://images.unsplash.com/photo-1499651681375-8afc5a4db253?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjIwOTIyfQ&s=eadad6ab3e4b92c6066ee4bfa73e9cc7q=85&fm=jpg&crop=faces&cs=srgb&w=100&h=100&fit=crop" class="mr-2 float-left rounded-circle" height="55" width="55">
