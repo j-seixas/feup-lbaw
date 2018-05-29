7bfedec9a8e276f79a7ab77aaa27ecb28540fdd4
JoseFreixo <josefreixo15@gmail.com>
2018-05-29 17:01:38 +0100

    Updated team members' descriptions

--- resources/views/pages/team.blade.php
+++ resources/views/pages/team.blade.php
@@ -14,10 +14,7 @@
             <img class="card-img-top" alt="Card image cap" src="https://avatars2.githubusercontent.com/u/19241121?s=400&v=4">
             <div class="card-body">
                 <h4 class="card-title">João Seixas</h4>
-                <p class="card-text">Palha</p>
-            </div>
-            <div class="card-footer">
-                <small class="text-muted">Last updated 3 mins ago</small>
+                <p class="card-text">SQL God</p>
             </div>
         </div>
         <div class="card">
@@ -26,28 +23,19 @@
                 <h4 class="card-title">José Freixo</h4>
                 <p class="card-text">Amante de Pokémons.</p>
             </div>
-            <div class="card-footer">
-                <small class="text-muted">Last updated 3 mins ago</small>
-            </div>
         </div>
         <div class="card">
             <img class="card-img-top" alt="Card image cap" src="https://avatars2.githubusercontent.com/u/6492427?s=400&v=4">
             <div class="card-body">
                 <h4 class="card-title">Rafael Damasceno</h4>
-                <p class="card-text">Palha</p>
-            </div>
-            <div class="card-footer">
-                <small class="text-muted">Last updated 3 mins ago</small>
+                <p class="card-text">Brasileiro</p>
             </div>
         </div>
         <div class="card">
             <img class="card-img-top" alt="Card image cap" src="https://avatars1.githubusercontent.com/u/19344861?s=400&v=4">
             <div class="card-body">
                 <h4 class="card-title">Tiago Carvalho</h4>
-                <p class="card-text">Palha</p>
-            </div>
-            <div class="card-footer">
-                <small class="text-muted">Last updated 3 mins ago</small>
+                <p class="card-text">A jogar POE</p>
             </div>
         </div>
     </div>
