0ca685c862829a30c1fff555eee89ed7b0e88378
rafaeldamasceno <rafaeldamasceno@me.com>
2018-05-29 10:37:15 +0100

    Fix bug with search. Show correct result type count.

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -10,8 +10,8 @@ class SearchController extends Controller
     public function search(Request $request) {
         $query = $request->input('query');
         $type = $request->input('type') ? $request->input('type') : 'event';
-        $events = DB::select('SELECT * FROM event WHERE to_tsvector(\'english\', title || \' \' || location) @@ to_tsquery(\'english\', ?) AND visibility = \'Public\'', [$query]);
-        $members = DB::select('SELECT * FROM member WHERE to_tsvector(\'english\', name) @@ to_tsquery(\'english\', ?)', [$query]);
-        return view('pages.search', ['events' => $events, 'members' => $members, 'type' => $type]);
+        $events = DB::select('SELECT * FROM event WHERE to_tsvector(\'english\', title || \' \' || location) @@ plainto_tsquery(\'english\', ?) AND visibility = \'Public\'', [$query]);
+        $members = DB::select('SELECT * FROM member WHERE to_tsvector(\'english\', name) @@ plainto_tsquery(\'english\', ?)', [$query]);
+        return view('pages.search', ['query' => $query, 'type' => $type, 'events' => $events, 'members' => $members]);
     }
 }
