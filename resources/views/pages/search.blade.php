e1b08e1db623406a1df228af7fa9ebdc38d2a632
rafaeldamasceno <rafaeldamasceno@me.com>
2018-05-29 15:32:57 +0100

    Oops, missed staging last commit's changes.

--- resources/views/pages/search.blade.php
+++ resources/views/pages/search.blade.php
@@ -50,25 +50,6 @@
                         <small class="text-muted">28 people going.</small>
                     </a>
                     @endforeach
-                    {{--<nav aria-label="...">   
-                        <ul class="pagination justify-content-center mt-0 pt-2">
-                            <li class="page-item disabled">
-                                <a class="page-link" href="#" tabindex="-1">Previous</a>
-                            </li>
-                            <li class="page-item">
-                                <a class="page-link" href="#">1</a>
-                            </li>
-                            <li class="page-item active">
-                                <a class="page-link" href="#">2 <span class="sr-only">(current)</span></a>
-                            </li>
-                            <li class="page-item">
-                                <a class="page-link" href="#">3</a>
-                            </li>
-                            <li class="page-item">
-                                <a class="page-link" href="#">Next</a>
-                            </li>
-                        </ul>
-                    </nav>--}}
                 </div>
             </div>
         </div>
